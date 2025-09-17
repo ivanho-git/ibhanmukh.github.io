f20e55f0c92d415b36a7c1a97c463d491b9f6960
Ibhan Mukherjee <ibhaanm29@gmail.com>
2025-09-18 00:37:31 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Optional JS for smooth scrolling
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
